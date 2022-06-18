5ab667bed681f8d95c1d5c7917e74034e7dad9bc
Sheraff <fpellet@ensc.fr>
2022-06-18 13:17:16 +0200

    pass tests

--- src/Logger/index.jsx
+++ src/Logger/index.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 import { useEffect, useRef } from "react";
 
 export default function Logger() {
-  const ref = useRef();
+  const ref = useRef(/** @type {HTMLDivElement} */(null));
   useEffect(() => {
     ref.current.classList.remove(styles.logger);
     ref.current.offsetWidth; // eslint-disable-line no-unused-expressions -- layout thrashing
